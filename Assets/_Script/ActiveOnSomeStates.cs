b2af52a5c7f2310fa27a6aa4d96f5e4427174add
Ana Clara Carneiro de Freitas <anaclara.2001@hotmail.com>
2022-02-20 18:01:20 -0300

    tutorial 2 feito

---
+++ Assets/_Script/ActiveOnSomeStates.cs
@@ -0,0 +1,29 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System.Linq;
+
+
+public class ActiveOnSomeStates : MonoBehaviour
+{
+    
+    public GameManager.GameState[] activeStates;
+    GameManager gm;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        GameManager.changeStateDelegate += UpdateVisibility;
+        gm = GameManager.GetInstance();
+        UpdateVisibility();
+    }
+
+
+    void UpdateVisibility(){
+        if(activeStates.Contains(gm.gameState)){
+            gameObject.SetActive(true);
+        }else{
+            gameObject.SetActive(false);
+        }
+    }
+}
--- Assets/_Script/MovimentoBola.cs
+++ Assets/_Script/MovimentoBola.cs
@@ -8,6 +8,9 @@ public class MovimentoBola : MonoBehaviour
     [Range(1,15)]
     public float velocidade = 5.0f;
     private Vector3 direcao;
+
+    GameManager gm;
+
     // Start is called before the first frame update
     void Start()
     {
@@ -15,11 +18,18 @@ public class MovimentoBola : MonoBehaviour
         float dirY = Random.Range(1.0f, 5.0f);
         direcao = new Vector3(dirX, dirY).normalized;
 
+        gm = GameManager.GetInstance();
+
     }
 
     // Update is called once per frame
     void Update()
     {
+
+        if(gm.gameState != GameManager.GameState.GAME){
+            return;
+        }
+
         transform.position += direcao*Time.deltaTime*velocidade;
         Vector2 posicaoViewport = Camera.main.WorldToViewportPoint(transform.position);
 
@@ -30,6 +40,27 @@ public class MovimentoBola : MonoBehaviour
         if( posicaoViewport.y < 0 || posicaoViewport.y > 1){
             direcao = new Vector3(direcao.x, -direcao.y);
         }
+
+        if(posicaoViewport.y < 0){
+            Reset();
+        }
+
+        Debug.Log($"Vidas: {gm.vidas} \t | \t Pontos: {gm.pontos}");
+    }
+
+    private void Reset(){
+        Vector3 playerPosition = GameObject.FindGameObjectWithTag("Player").transform.position;
+        transform.position = playerPosition + new Vector3(0, 0.5f, 0);
+
+        float dirX = Random.Range(-5.0f, 5.0f);
+        float dirY = Random.Range(2.0f, 5.0f);
+
+        direcao = new Vector3(dirX, dirY).normalized;
+        gm.vidas--;
+
+        if(gm.vidas <= 0 && gm.gameState == GameManager.GameState.GAME){
+            gm.ChangeState(GameManager.GameState.ENDGAME);
+        }
     }
 
     void OnTriggerEnter2D(Collider2D col){
@@ -39,8 +70,10 @@ public class MovimentoBola : MonoBehaviour
             float dirY = Random.Range(1.0f, 5.0f);
             direcao = new Vector3(dirX, dirY).normalized;
         } 
+
         else if (col.gameObject.CompareTag("Tijolo")){
             direcao = new Vector3(direcao.x, -direcao.y);
+            gm.pontos++;
         }
 
     }
